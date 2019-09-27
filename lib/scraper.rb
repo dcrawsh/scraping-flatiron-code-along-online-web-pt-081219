ce8c86fb70a910ed5f1cb35837fd3e91cd096b23
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-27 02:40:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -16,7 +16,7 @@ class Scraper
   def make_courses
   course = Course.new 
   course.title = self.get_courses.css("article h2")
-  course.schedule = self.get_courses.css("article ")
+  course.schedule = self.get_courses.css("article  ")
   end 
   
   def print_courses

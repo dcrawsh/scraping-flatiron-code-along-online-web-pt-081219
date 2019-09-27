877b7980d8442e0f7e9e1158fa6fa1b711ee38b1
Dylan Crawshaw <dcrawsh@gmail.com>
2019-09-27 02:47:52 +0000

    Done.

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,18 @@
+class Course 
+attr_accessor :title, :schedule, :description 
+@@all = Array.new 
 
+def initialize  
+Course.all << self 
+end 
+
+def self.all 
+@@all
+end 
+  
+def self.reset_all 
+self.all.clear 
+end 
+
+end 
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,23 @@ require_relative './course.rb'
 
 class Scraper
   
+  def get_page 
+  data = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end 
+  
+  def get_courses
+  self.get_page.css("article.post")
+  end 
+  
+  def make_courses
+  self.get_courses.map do |courses|
+  course = Course.new 
+  course.title = courses.css("article h2").text
+  course.schedule = courses.css("article em.date").text
+  course.description = courses.css("article p").text
+  end 
+end 
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
